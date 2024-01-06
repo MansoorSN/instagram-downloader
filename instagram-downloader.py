85b5c6c76d83ac0962016fa0b087f3dbc57669f1
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2024-01-06 18:25:03 -0500

    Update instagram-downloader.py

--- instagram-downloader.py
+++ instagram-downloader.py
@@ -10,7 +10,7 @@ def init_instaloader():
     return instaloader.Instaloader()
 
 L = init_instaloader()
-L.login('USERNAME','PASSWORD')
+L.login(USERNAME, PASSWORD)
 
 
 st.title("Instagram Downloader")
