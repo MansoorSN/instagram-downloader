4474807840f0d8ef8b0ab85d0d27419a67e17404
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2024-01-06 19:20:25 -0500

    Update instagram-downloader.py

--- instagram-downloader.py
+++ instagram-downloader.py
@@ -10,13 +10,7 @@ def init_instaloader():
     return instaloader.Instaloader()
 
 L = init_instaloader()
-#L.login(st.secrets["USERNAME"], st.secrets["PASSWORD"])
 
-try:
-    L.load_session_from_file(st.secrets["USERNAME"], st.secrets["USERNAME"] + st.secrets["PASSWORD"])
-except:
-    L.login(st.secrets["USERNAME"], st.secrets["PASSWORD"])
-    L.save_session_to_file(st.secrets["USERNAME"] + st.secrets["PASSWORD"])
 
 st.title("Instagram Downloader")
 st.markdown('''
